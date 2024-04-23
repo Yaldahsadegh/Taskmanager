585062c3a64e18b96ecc8d6477f7d713cd2dcb68
Yalda Hajisadegh <eyaha001@edu.xamk.fi>
2024-04-23 13:20:13 +0300

    basic view task route add

--- frontend/src/components/TaskView.tsx
+++ frontend/src/components/TaskView.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container, Card } from "react-bootstrap";
 
 const TaskView = () => {
   interface Task {
@@ -19,12 +21,27 @@ const TaskView = () => {
     axios
       .get(`http://localhost:4000/task/${params.id}`)
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data.task[0]);
+        setTask(response.data.task[0]);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>TaskView</div>;
+  return (
+    <Container>
+      {task ? (
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>{task.name}</Card.Title>
+            <Card.Text>{task.content}</Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      ) : (
+        <p>No task to display</p>
+      )}
+    </Container>
+  );
 };
 
 export default TaskView;
